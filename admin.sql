14661af3e1ac1dad10b384cdfe8278740ceb4d6a
IstvanW <55334150+IstvanW@users.noreply.github.com>
2020-11-29 21:10:04 -0600

    Add files via upload

--- admin.sql
+++ admin.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Nov 11, 2020 at 04:06 AM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 30, 2020 at 03:09 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,6 +34,32 @@ CREATE TABLE IF NOT EXISTS `link` (
   PRIMARY KEY (`url`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `link`
+--
+
+INSERT INTO `link` (`url`, `department`) VALUES
+('supportDptLinks/manage_user_acct.html', 1),
+('supportDptLinks/assign_roles.html', 1),
+('supportDptLinks/help_desk.html', 1),
+('financeDptLinks/finance_reports.html', 2),
+('financeDptLinks/accounts_payable.html', 2),
+('financeDptLinks/accounts_receivable.html', 2),
+('financeDptLinks/tax.html', 2),
+('salesDptLinks/sales_report.html', 3),
+('salesDptLinks/sales_leads.html', 3),
+('salesDptLinks/sales_demo.html', 3),
+('hrDptLinks/new_hire.html', 4),
+('hrDptLinks/benefits.html', 4),
+('hrDptLinks/payroll.html', 4),
+('hrDptLinks/off_boarding.html', 4),
+('hrDptLinks/hr_reports.html', 4),
+('techDptLinks/app_monitoring.html', 5),
+('techDptLinks/tech_support.html', 5),
+('techDptLinks/app_dev.html', 5),
+('techDptLinks/app_admin.html', 5),
+('techDptLinks/release_management.html', 5);
+
 -- --------------------------------------------------------
 
 --
@@ -45,7 +70,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(200) NOT NULL AUTO_INCREMENT,
   `username` varchar(15) NOT NULL,
-  `pass` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `pass` varchar(15) NOT NULL,
   `department` int(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
